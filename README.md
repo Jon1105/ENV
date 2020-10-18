c8e9448fed34af8de92a0a28b688b7f4f9d367e0
Jonathan Ramanantsoa <jonathan.rama2005@gmail.com>
2020-10-18 15:29:43 +0800

    intial commit

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # ENV 
+Short script which allows to read 'environment' variables from .`.ini` and `.env` files
+
+### Usage
+```
+import env
+
+password = env.get_env('PASSWORD', './.env')
+```
+
+The second parameter (path) is optional
+If it is not provided, the script will search the current directory for and `.ini` or `.env` files
+
+`get_env('Invalid variable name')` will return `None`
+
---
+++ env.py
@@ -0,0 +1,36 @@
+import os
+
+
+def get_env(var_name, filePath=None):
+    if filePath:
+        ret = __get_var(var_name, filePath)
+    else:
+        if os.path.isfile('./.env'):
+            ret = __get_var(var_name, './.env')
+        elif os.path.isfile('./.ini'):
+            ret = __get_var(var_name, './.ini')
+        else:
+            for f in os.listdir('./'):
+                if f.endswith('.ini') or f.endswith('.env'):
+                    ret = __get_var(var_name, f)
+    return ret
+
+
+def __get_var(var_name, filePath):
+    with open(filePath, 'r') as f:
+        for line in f.readlines():
+            if var_name in line:
+                index = line.find('=')
+                if index < 1:
+                    continue
+                value = line[index+1: len(line) - 1]
+                if value.strip().lower() == 'true':
+                    return True
+                elif value.strip().lower() == 'false':
+                    return False
+                else:
+                    return value
+        return None
+
+
+print(get_env('boolean', 'C:\\Users\\jonat\\OneDrive\\Documents\\Programming\\Node.js\\Course'))
