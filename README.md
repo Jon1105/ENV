aeb3cc90a01fbd7af5dd83656ee0a19531fdd7d8
Jonathan Ramanantsoa <jonathan.rama2005@gmail.com>
2020-10-18 15:58:11 +0800

    Improved whitespace removal functionality

--- README.md
+++ README.md
@@ -5,15 +5,20 @@ Short script which allows to read 'environment' variables from .`.ini` and `.env
 ```
 import env
 
-password = env.get_env('PASSWORD', './.env')
+password = env.get_env('PASSWORD', './.env', True)
 ```
 
 The second parameter (path) is optional
 If it is not provided, the script will search the current directory for and `.ini` or `.env` files
 
+The third parameter (strip whitespaces) is optional as well and defaults to a True  
+If values contain trailing or leading whitespaces, set this to False
+
+
+
 `get_env('Invalid variable name')` will return `None`
 
 ** File format of `.env` file from example above:
 ```
-PASSWORD=my_super_secret_password
+PASSWORD = my_super_secret_password
 ```
--- env.py
+++ env.py
@@ -1,32 +1,33 @@
 import os
 
 
-def get_env(var_name, filePath=None):
+def get_env(var_name, filePath=None, stripSpace=True):
     if filePath:
-        ret = __get_var(var_name, filePath)
+        ret = __get_var(var_name, filePath, stripSpace)
     else:
         if os.path.isfile('./.env'):
-            ret = __get_var(var_name, './.env')
+            ret = __get_var(var_name, './.env', stripSpace)
         elif os.path.isfile('./.ini'):
-            ret = __get_var(var_name, './.ini')
+            ret = __get_var(var_name, './.ini', stripSpace)
         else:
             for f in os.listdir('./'):
                 if f.endswith('.ini') or f.endswith('.env'):
-                    ret = __get_var(var_name, f)
+                    ret = __get_var(var_name, f, stripSpace)
     return ret
 
 
-def __get_var(var_name, filePath):
+def __get_var(var_name, filePath, stripSpace=True):
     with open(filePath, 'r') as f:
         for line in f.readlines():
-            if var_name in line:
+            if line.startswith(var_name):
                 index = line.find('=')
                 if index < 1:
                     continue
                 value = line[index+1: len(line) - 1]
-                if value.strip().lower() == 'true':
+                value = value.strip(' ' if stripSpace else '')
+                if value.lower() == 'true':
                     return True
-                elif value.strip().lower() == 'false':
+                elif value.lower() == 'false':
                     return False
                 else:
                     return value
